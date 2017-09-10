b5edec81fbbacd1b3d58d7970a41049a314ba360
Yasmim <you@example.com>
2017-09-10 00:19:41 -0300

    Arrumando diretórios

--- projeto1Web/js/init.js
+++
@@ -1,8 +0,0 @@
-(function($){
-  $(document).ready(function() {
-    Materialize.updateTextFields();
-  });
-  $(document).ready(function(){
-    $('ul.tabs').tabs('select_tab', 'tab_id');
-  })
-})(jQuery); // end of jQuery name space
